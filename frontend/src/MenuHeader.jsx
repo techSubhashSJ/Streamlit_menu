1591ed80d1b5eaaa10396b7caae095f9933df80e
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-12-12 22:28:33 +0530

    added menu header

---
+++ frontend/src/MenuHeader.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { styled, useStyletron } from "baseui";
+
+const MenuHeader = (props) => {
+  const { appLogo, appName, styles } = props;
+
+  const [css] = useStyletron();
+
+  return (
+    <Logo
+      justify={styles.justify}
+      fontFamily={styles.fontFamily}
+      color={styles.color}
+    >
+      <img
+        className={css({
+          width: "2rem",
+          height: "2rem",
+          marginRight: "0.5rem",
+        })}
+        src={`data:image/png;base64, ${appLogo}`}
+        alt="logo"
+      />
+      {appName}
+    </Logo>
+  );
+};
+
+export default MenuHeader;
+
+const Logo = styled("div", (props) => ({
+  padding: "1rem 2rem",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: props.justify ? props.justify: "center",
+  fontFamily: props.fontFamily ?? '"Times New Roman", Times, serif',
+  fontSize: "1.25rem",
+  fontWeight: "bold",
+  boxSizing: "border-box",
+  background: "none",
+  color: props.color && props.color !== "" ? props.color : "black",
+}));
