54b1a911c33fdccae1a501a737ba9ae44d5d52ab
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-12-12 22:50:54 +0530

    menu related data added

---
+++ assets/menu_data/menu.py
@@ -0,0 +1,66 @@
+import assets.icons.setting
+
+menuData = [
+    {
+        "id": 1,
+        "title": "Overview",
+        "icon": assets.icons.setting.setting_icon,
+        "child": "None",
+    },
+    {
+        "id": 2,
+        "title": "Tickets",
+        "icon": assets.icons.setting.setting_icon,
+        "child": "None",
+    },
+    {
+        "id": 3,
+        "title": "Ideas",
+        "icon": assets.icons.setting.setting_icon,
+        "child": [
+            {
+                "id": 4,
+                "title": "Submenu1",
+                "icon": assets.icons.setting.setting_icon,
+                "child": "None",
+            },
+            {
+                "id": 5,
+                "title": "Submenu2",
+                "icon": assets.icons.setting.setting_icon,
+                "child": "None",
+            },
+        ],
+    },
+    {
+        "id": 6,
+        "title": "Contacts",
+        "icon": assets.icons.setting.setting_icon,
+        "child": "None",
+    },
+    {
+        "id": 7,
+        "title": "Agents",
+        "icon": assets.icons.setting.setting_icon,
+        "child": "None",
+    },
+    {
+        "id": 8,
+        "title": "Articles",
+        "icon": assets.icons.setting.setting_icon,
+        "child": [
+            {
+                "id": 9,
+                "title": "Submenu1",
+                "icon": assets.icons.setting.setting_icon,
+                "child": "None",
+            },
+            {
+                "id": 10,
+                "title": "Submenu2",
+                "icon": assets.icons.setting.setting_icon,
+                "child": "None",
+            },
+        ],
+    },
+]
