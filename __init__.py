1a09dfaf27071e965056e1fc1bc5776f3e88a103
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-12-15 11:00:42 +0530

    separated streamlit init file

--- __init__.py
+++ __init__.py
@@ -1,44 +1,8 @@
-import os
-import streamlit.components.v1 as components
 import assets.app_logo.logo
 import assets.menu_data.menu
 from register import register_callback, init
 
-# Create a _RELEASE constant. We'll set this to False while we're developing
-# the component, and True when we're ready to package and distribute it.
-# (This is, of course, optional - there are innumerable ways to manage your
-# release process.)
-_RELEASE = False
-
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
-
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
-
-if not _RELEASE:
-    _component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("my_component"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
-        "Menu",
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
-        url="http://localhost:3000",
-    )
-else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to to the component's
-    # build directory:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("Menu", path=build_dir)
+from main import _component_func, _RELEASE
 
 # Create a wrapper function for the component. This is an optional
 # best practice - we could simply expose the component function returned by
@@ -48,8 +12,7 @@ else:
 
 init()
 
-
-def myFunc(menu, collapsable="true", key=None, on_select=None, args: tuple = ()):
+def getMenu(menu, collapsable="true", key=None, on_select=None, args: tuple = ()):
     register_callback(key, on_select, *args)
 
     component_value = _component_func(menu=menu,
@@ -104,16 +67,19 @@ if not _RELEASE:
         # print(st.session_state[widgetkey])
         pass
 
+    # Overridden inbuilt streamlit css 
     with open("style.css") as stylefile:
         st.markdown(f"<style>{stylefile.read()}</style>",
                     unsafe_allow_html=True)
 
-    with st.sidebar:
-        if "sidemenu" in st.session_state:
-            # st.write(f"""Hi {st.session_state["sidemenu"]}""", )
-            pass
+    column_left, column_center, column_right = st.columns(3)
 
-        myFunc(menu, collapsable="true",
+    with column_center:
+        getMenu(menu, collapsable="true",
                key="sidemenu", on_select=on_menu_select, args=("sidemenu",))
 
-    st.write('You have selected: ', st.session_state["sidemenu"])
+    with column_left:
+        st.write('You have selected: ', st.session_state["sidemenu"])
+
+    with column_right:
+        st.write("right side")
