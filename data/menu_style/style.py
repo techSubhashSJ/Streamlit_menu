cabd03686692e36dee00d680576abfcdd2f8b712
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-12-28 16:22:55 +0530

    Test production

--- data/menu_style/style.py
+++ data/menu_style/style.py
@@ -8,45 +8,45 @@ submenuStyle = {}
 mainMenuStyle = {}
 
 #custom theme
-# menuHeaderStyle = {
-#     "flexDirection": "column",
-#     "justify": "center",
-#     "fontFamily": "'Brush Script MT', cursive",
-#     "color": "#32373d",
-#     "background":
-#     "url(https://colorlib.com/etc/bootstrap-sidebar/sidebar-09/images/bg_1.jpg) center",
-#     "fontSize": "2.75rem",
-#     "height": "10rem",
-#     "logo": {
-#         "borderRadius": "100px",
-#         "width": "3rem",
-#         "height": "3rem"
-#     }
-# }
-# menuWrapperStyle = {"backgroundColor": "#32373d"}
+menuHeaderStyle = {
+    "flexDirection": "column",
+    "justify": "center",
+    "fontFamily": "'Brush Script MT', cursive",
+    "color": "#32373d",
+    "background":
+    "url(https://colorlib.com/etc/bootstrap-sidebar/sidebar-09/images/bg_1.jpg) center",
+    "fontSize": "2.75rem",
+    "height": "10rem",
+    "logo": {
+        "borderRadius": "100px",
+        "width": "3rem",
+        "height": "3rem"
+    }
+}
+menuWrapperStyle = {"backgroundColor": "#32373d"}
 
-# submenuStyle = {
-#     "color": "rgba(255,255,255,.6)",
-#     "fontFamily": "'Courier New', monospace",
-#     "hover": {
-#         "color": "#fff",
-#         "backgroundColor": "#2f89fc",
-#         "leftBorder":False
-#     },
-#     "activeSubMenu": {
-#         "color": "#fff",
-#     }
-# }
+submenuStyle = {
+    "color": "rgba(255,255,255,.6)",
+    "fontFamily": "'Courier New', monospace",
+    "hover": {
+        "color": "#fff",
+        "backgroundColor": "#2f89fc",
+        "leftBorder":False
+    },
+    "activeSubMenu": {
+        "color": "#fff",
+    }
+}
 
-# mainMenuStyle = {
-#     "color": "rgba(255,255,255,.6)",
-#     "fontFamily": "'Courier New', monospace",
-#     "hover": {
-#         "color": "#fff",
-#         "backgroundColor": "#2f89fc",
-#         "leftBorder":False
-#     },
-#     "activeMenu": {
-#         "color": "#fff",
-#     }
-# }
+mainMenuStyle = {
+    "color": "rgba(255,255,255,.6)",
+    "fontFamily": "'Courier New', monospace",
+    "hover": {
+        "color": "#fff",
+        "backgroundColor": "#2f89fc",
+        "leftBorder":False
+    },
+    "activeMenu": {
+        "color": "#fff",
+    }
+}
